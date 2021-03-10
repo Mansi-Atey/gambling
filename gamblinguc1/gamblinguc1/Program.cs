044f4a2dafd39b7503f7b357f9a3fa5a6c52a14f
Mansi-Atey <mansiatey31@gmail.com>
2021-03-10 12:32:05 +0530

    [mansi]ADD: player won or loose

--- gamblinguc1/gamblinguc1/Program.cs
+++ gamblinguc1/gamblinguc1/Program.cs
@@ -4,14 +4,23 @@ namespace gamblinguc1
 {
     class Program
     {
-        public int cash = 100;
+        public int stake = 100;
         public int bet = 1;
         static void Main(string[] args)
-        { 
-          
-       
-            
-         }
+        {
 
+            Random random = new Random();
+            int checkVal = Random.Next(0, 2);
+            if (checkVal == 1)
+            {
+                Console.WriteLine("player won");
+            }
+            else
+            {
+                Console.WriteLine("player loose");
+
+            }
+
+        }
     }
 }
