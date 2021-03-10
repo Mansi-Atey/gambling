93ef088d526dd7c0d8f70a0b5e09154c1869bf3a
Mansi-Atey <mansiatey31@gmail.com>
2021-03-10 12:04:19 +0530

    [mansi]ADD: initialize variables

---
+++ gamblinguc1/gamblinguc1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace gamblinguc1
+{
+    class Program
+    {
+        public int cash = 100;
+        public int bet = 1;
+        static void Main(string[] args)
+        { 
+          
+       
+            
+         }
+
+    }
+}
